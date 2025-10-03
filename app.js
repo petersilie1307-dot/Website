d2eca4189e6c1bba6e3d4d254e923fb0e2707e72
petersilie1307-dot <petersilie1307@gmail.com>
2025-10-03 12:01:17 +0200

    a

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+let count = 0;
+const cookieBtn = document.getElementById('cookie');
+const countSpan = document.getElementById('count');
+
+cookieBtn.addEventListener('click', () => {
+  count++;
+  countSpan.textContent = count;
+});
 
